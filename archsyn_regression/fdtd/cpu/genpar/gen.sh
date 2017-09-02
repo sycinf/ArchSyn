892d00c24f8f30a4987d4d69086eb684acfac1a3
Shaoyi Cheng <cheng@numericcal.com>
2017-09-02 05:42:46 -0700

    add the flag so it performs dependency analysis only

---
+++ archsyn_regression/fdtd/cpu/genpar/gen.sh
@@ -0,0 +1,2 @@
+source ../../../install_dir.sh
+$LLVM_BIN_INSTALL_DIR/gen-par  ../../fdtd_mll.bc   -o pared.ll 
--- archsyn_regression/fdtd_const/cpu/genpar/gen.sh
+++ archsyn_regression/fdtd_const/cpu/genpar/gen.sh
@@ -1,4 +1,4 @@
 source ../../../install_dir.sh
-$LLVM_BIN_INSTALL_DIR/gen-par  ../../fdtdconst.bc  -o dump.ll 
+$LLVM_BIN_INSTALL_DIR/gen-par  ../../fdtdconst.bc -DAInfo  -o dump.ll 
 echo "---------------------------Finished analyzing const substituted kernels, starting on original kernel-------------------" 
-$LLVM_BIN_INSTALL_DIR/gen-par  ../../fdtd.bc  -o dump2.ll  
+$LLVM_BIN_INSTALL_DIR/gen-par  ../../fdtd.bc  -DAInfo -o dump2.ll  
